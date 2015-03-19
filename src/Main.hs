a06931bc33d3b327a517e57d0d2e165f3f82d685
Alex Ignatenko <alex@getbase.com>
2015-03-19 23:57:07 +0100

    Print json of user

--- src/Main.hs
+++ src/Main.hs
@@ -4,12 +4,16 @@ module Main where
 
 import Data.Text
 import Data.Aeson
-import Data.Default
-import Control.Applicative
--- import Control.Monad
+-- import Data.Default
+import Data.Conduit
+import qualified Data.Conduit.Binary as CB
+-- import Control.Applicative
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Resource
 import GHC.Generics
-import Network.HTTP.Conduit (simpleHttp, newManager)
+import Network.HTTP.Conduit
 import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Char8 as BS
 
 username :: String
 username = "mojombo"
@@ -31,12 +35,31 @@ instance ToJSON User
 adminText :: User -> String
 adminText user = if site_admin user then "Admin" else "Normal"
 
+parseSink :: Sink BS.ByteString (ResourceT IO) ()
+parseSink = do
+    md <- await
+    case md of
+        Nothing -> return ()
+        Just smth -> do
+            liftIO $ BS.putStrLn smth
+            parseSink
+
 main :: IO ()
 main = do
-    -- json <- (eitherDecode <$> simpleHttp $ (usersUrl ++ username)) :: IO (Either String User)
-    json <- (eitherDecode <$> getUser) :: IO (Either String User)
-    case json of
-        Left error -> putStrLn error
-        Right user -> print user
+    manager <- newManager conduitManagerSettings
+    request <- parseUrl $ usersUrl ++ username
+    let headers = requestHeaders request
+        request' = request {
+            requestHeaders =
+                ("User-agent", "haskell-bot") : headers
+        }
+    runResourceT $ do
+        response <- http request' manager
+        responseBody response $$+- CB.lines =$ parseSink
+   
 
+    -- json <- (eitherDecode <$> getUser) :: IO (Either String User)
+    -- case json of
+    --     Left error -> putStrLn error
+    --     Right user -> print user
 
