2416d048187b8397afbab161a0104d29c9c14a99
Alex Ignatenko <alex@getbase.com>
2015-04-12 15:32:51 +0200

    Get github user through obtainUser

--- src/Main.hs
+++ src/Main.hs
@@ -4,14 +4,37 @@ module Main where
 
 import Control.Exception
 import Network.HTTP.Conduit
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Trans.Resource
+-- import Control.Monad.IO.Class (liftIO)
+-- import Control.Monad.Trans.Resource
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Text as Text
+import Data.Text.Encoding (encodeUtf8, decodeUtf8)
 
 type User = String
 
 database :: FilePath
 database = "database"
+
+defaultUsername :: String
+defaultUsername = "mojombo"
+
+usersUrl :: String
+usersUrl = "https://api.github.com/users/"
+
+userAgent :: BS.ByteString
+userAgent = "haskell-bot"
+
+bytes = encodeUtf8 . Text.pack
+string = Text.unpack . decodeUtf8
+strict = BL.toStrict
+
+setUserAgent :: Request -> BS.ByteString -> Request
+setUserAgent request agent =
+    let headers = requestHeaders request
+    in request {
+            requestHeaders = ("User-agent", agent) : headers
+       }
 
 tryReadFile :: FilePath -> IO (Either IOException String)
 tryReadFile path = try $ readFile path
@@ -24,12 +47,17 @@ readFileMaybe path =
             Right content -> return $ Just content
 
 -- fetch from local store
-fetchUser :: Int -> IO (Maybe User)
+fetchUser :: String -> IO (Maybe User)
 fetchUser _ = readFileMaybe database
 
 -- get from web service
-getUser :: Int -> IO (Maybe User)
-getUser id = return $ Just $ "pooh" ++ " " ++ show id
+getUser :: String -> IO (Maybe User)
+getUser username = withManager $ \manager -> do
+    -- TODO: check for errors in response
+    initRequest <- parseUrl $ usersUrl ++ username
+    request <- return $ setUserAgent initRequest userAgent
+    response <- httpLbs request manager
+    return $ Just $ string $ strict $ responseBody response
 
 -- write to local store and return
 writeUser :: Maybe User -> IO (Maybe User)
@@ -38,33 +66,17 @@ writeUser maybeUser =
         Just user -> writeFile database user >> return maybeUser
         Nothing -> return maybeUser
 
-obtainUser :: Int -> IO (Maybe User)
-obtainUser id = 
-    fetchUser id >>= \maybeUser ->
+obtainUser :: String -> IO (Maybe User)
+obtainUser username = 
+    fetchUser username >>= \maybeUser ->
         case maybeUser of
             Just user -> return $ Just user
-            Nothing -> getUser id >>= writeUser
-
-username :: String
-username = "mojombo"
-
-usersUrl :: String
-usersUrl = "https://api.github.com/users/"
-
-userAgent :: BS.ByteString
-userAgent = "haskell-bot"
-
-setUserAgent :: Request -> BS.ByteString -> Request
-setUserAgent request agent =
-    let headers = requestHeaders request
-    in request {
-            requestHeaders = ("User-agent", agent) : headers
-       }
+            Nothing -> getUser username >>= writeUser
 
 main :: IO ()
-main = withManager $ \manager -> do
-    initRequest <- parseUrl $ usersUrl ++ username
-    request <- return $ setUserAgent initRequest userAgent
-    response <- httpLbs request manager
-    liftIO $ print $ responseBody response
+main = obtainUser defaultUsername >>= \user ->
+    case user of
+        Nothing -> putStrLn "Nothing"
+        Just user -> putStrLn user
+   
 
