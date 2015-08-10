5f62e343a4984ed2c3a4d48afb28fa8e2d087176
Alex Ignatenko <alex@getbase.com>
2015-08-10 21:26:13 +0200

    Configure request to not throw exception + playing around with writing 'try' functions

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,7 @@ module Main where
 
 import Control.Exception
 import Network.HTTP.Conduit
--- import Control.Monad.IO.Class (liftIO)
+import Control.Monad.IO.Class
 -- import Control.Monad.Trans.Resource
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy as BL
@@ -20,7 +20,7 @@ defaultUsername :: String
 defaultUsername = "mojombo"
 
 usersUrl :: String
-usersUrl = "https://api.github.com/users/"
+usersUrl = "https://api.github.com/users_/"
 
 userAgent :: BS.ByteString
 userAgent = "haskell-bot"
@@ -36,6 +36,10 @@ setUserAgent request agent =
             requestHeaders = ("User-agent", agent) : headers
        }
 
+setDontCheckStatus :: Request -> Request
+setDontCheckStatus request =
+    request { checkStatus = \_ _ _ -> Nothing }
+
 tryReadFile :: FilePath -> IO (Either IOException String)
 tryReadFile path = try $ readFile path
 
@@ -46,18 +50,52 @@ readFileMaybe path =
             Left _ -> return Nothing
             Right content -> return $ Just content
 
+stringResponseBody :: Response BL.ByteString -> String
+stringResponseBody = string . strict . responseBody
+
+-- tryStatus :: StatusCodeException e => IO a -> IO (Either e a)
+-- tryStatus a = 
+--     catch (a >>= \ v -> return (Right v)) (\e -> return (Left e))
+
+tryHttpLbs
+    :: MonadIO m
+    => Request 
+    -> Manager 
+    -> m (Either IOException (Response BL.ByteString))
+tryHttpLbs request manager = liftIO $ try $ httpLbs request manager
+
+-- maybeHttpLbs
+--     :: MonadIO m
+--     => Request
+--     -> Manager
+--     -> m (Maybe (Response BL.ByteString))
+-- maybeHttpLbs request manager =
+--     httpLbs request manager
+--    (return $ Just (httpLbs request manager)) `catch`
+--        \(StatusCodeException s _ _) -> return $ Nothing
+
 -- fetch from local store
 fetchUser :: String -> IO (Maybe User)
 fetchUser _ = readFileMaybe database
+
+configureRequest :: Request -> Request
+configureRequest request = setDontCheckStatus $ setUserAgent request userAgent
 
 -- get from web service
 getUser :: String -> IO (Maybe User)
 getUser username = withManager $ \manager -> do
     -- TODO: check for errors in response
-    initRequest <- parseUrl $ usersUrl ++ username
-    request <- return $ setUserAgent initRequest userAgent
+    request' <- parseUrl $ usersUrl ++ username
+    -- let request = setUserAgent request' userAgent
+    let request = configureRequest request'
+
     response <- httpLbs request manager
-    return $ Just $ string $ strict $ responseBody response
+    return $ Just $ stringResponseBody response
+
+    -- either <- tryHttpLbs request manager
+    -- case either of
+    --     Right response -> return $ Just $ stringResponseBody response
+    --     Left _ -> return Nothing
 
 -- write to local store and return
 writeUser :: Maybe User -> IO (Maybe User)
