59e2acaaaccf757af4e38f86c231595ca6d2aacc
Alex Ignatenko <alex@getbase.com>
2015-09-04 17:30:20 +0200

    Parse User record

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,7 @@ import Data.Aeson
 import Data.Text
 -- import Control.Applicative
 import GHC.Generics
-import Control.Exception
+import qualified Control.Exception as EX
 import Network.HTTP.Conduit
 import Network.HTTP.Types.Status
 -- import Control.Monad.IO.Class
@@ -16,6 +16,7 @@ import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as Text
 import Data.Text.Encoding (encodeUtf8, decodeUtf8)
+import Control.Monad.Catch
 
 type JSONString = BS.ByteString
 
@@ -28,12 +29,16 @@ defaultUsername = "mojombo"
 usersUrl :: String
 usersUrl = "https://api.github.com/users/"
 
+reposUrlPart :: String
+reposUrlPart = "repos"
+
 userAgent :: BS.ByteString
 userAgent = "haskell-bot"
 
 bytes = encodeUtf8 . Text.pack
 string = Text.unpack . decodeUtf8
 strict = BL.toStrict
+lazy = BL.fromStrict
 
 setUserAgent :: Request -> BS.ByteString -> Request
 setUserAgent request agent =
@@ -46,8 +51,8 @@ setDontCheckStatus :: Request -> Request
 setDontCheckStatus request =
     request { checkStatus = \_ _ _ -> Nothing }
 
-tryReadFile :: FilePath -> IO (Either IOException BS.ByteString)
-tryReadFile path = try $ BS.readFile path
+tryReadFile :: FilePath -> IO (Either EX.IOException BS.ByteString)
+tryReadFile path = EX.try $ BS.readFile path
 
 readFileMaybe :: FilePath -> IO (Maybe BS.ByteString)
 readFileMaybe path =
@@ -66,21 +71,37 @@ strictResponseBody = strict . responseBody
 fetchUser :: String -> IO (Maybe JSONString)
 fetchUser _ = readFileMaybe database
 
+-- get from web service
+getUser :: String -> IO (Maybe JSONString)
+getUser username = runGetRequest $ usersUrl ++ username
+
+getUsers :: IO (Maybe JSONString)
+getUsers = runGetRequest $ usersUrl
+
+getRepo :: String -> IO (Maybe JSONString)
+getRepo username = runGetRequest $ usersUrl ++ "/" ++ username ++ "/" ++ reposUrlPart
+
+runGetRequest :: String -> IO (Maybe JSONString)
+runGetRequest url = do
+    request <- makeRequest url
+    runRequest request
+
 configureRequest :: Request -> Request
 configureRequest request = setDontCheckStatus $ setUserAgent request userAgent
 
--- get from web service
-getUser :: String -> IO (Maybe JSONString)
-getUser username = withManager $ \manager -> do
-    request' <- parseUrl $ usersUrl ++ username
-    let request = configureRequest request'
+makeRequest :: MonadThrow m => String -> m Request
+makeRequest url = do
+    request' <- parseUrl url
+    return $ configureRequest request'
 
+runRequest :: Request -> IO (Maybe JSONString)
+runRequest request = withManager $ \manager -> do
     response <- httpLbs request manager
     statusCode <- return $ statusCode $ responseStatus response
     if 200 <= statusCode && statusCode < 300
         then return $ Just $ strictResponseBody response
         else return Nothing
-
+ 
 -- write to local store and return
 writeUser :: Maybe JSONString -> IO (Maybe JSONString)
 writeUser maybeUser =
@@ -95,27 +116,23 @@ obtainUser username =
             Just user -> return $ Just user
             Nothing -> getUser username >>= writeUser
 
+data User = User { id           :: Int
+                 , login        :: !Text
+                 , avatar_url   :: !Text
+                 , site_admin   :: Bool
+                 } deriving (Show, Generic)
 
-data GHUser = GHUser { id           :: Int
-                     , login        :: !Text
-                     , avatar_url   :: !Text
-                     , site_admin   :: Bool
-                     } deriving (Show, Generic)
+instance FromJSON User
+instance ToJSON User
 
-instance FromJSON GHUser
-instance ToJSON GHUser
-
-parseUser :: Maybe BL.ByteString -> IO (Either String GHUser)
-parseUser json =
-    case json of
-        Nothing -> return $ Left "Empty JSON is given"
-        Just json -> return $ eitherDecode json
+parseUser :: BL.ByteString -> Either String User
+parseUser json = eitherDecode json
 
 main :: IO ()
 main = obtainUser defaultUsername >>= \user ->
     case user of
-        Nothing -> putStrLn "Nothing"
-        Just user -> putStrLn $ string user
+        Nothing -> print "Nothing"
+        Just user -> print $ parseUser $ lazy user
 
 -- Add promt and repl functions for future use
 prompt :: String -> IO String
